66038b297689b05ea2929fe304c5f500ea99c316
Netaly Ramirez <21netaly@gmail.com>
2020-03-23 23:57:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  return "Hi, my name is #{name}."
+   "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
-  return "Hi, my name is #{name} and I am learning to program  in #{language}."
+   "Hi, my name is #{name} and I am learning to program  in #{language}."
 end
